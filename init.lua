23c0a3ebcd48372f60b14ff910f0e12688b9bb3d
Minh Ha <66556512+haminh7036@users.noreply.github.com>
2026-07-02 17:39:23 +0700

    chore: initial commit

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- Leader Key
+vim.g.mapleader = " "
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Line Number
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Highlight Current Line
+vim.opt.cursorline = true
