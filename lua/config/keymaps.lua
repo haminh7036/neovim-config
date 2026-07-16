69337620de583fdd94a78bfe2c8ca5ed26f47dc7
Minh Ha <minhha7036@gmail.com>
2026-07-17 00:54:04 +0700

    feat: add which-key plugin, configure C-hjkl split navigation and filters

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,13 @@
 vim.g.mapleader = " "
+
+-- Di chuyển giữa các cửa sổ (Split Navigation) bằng Ctrl + hjkl
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window" })
+
+-- Di chuyển giữa các cửa sổ bằng Ctrl + các phím mũi tên
+vim.keymap.set("n", "<C-Left>", "<C-w>h", { desc = "Go to Left Window" })
+vim.keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower Window" })
+vim.keymap.set("n", "<C-Up>", "<C-w>k", { desc = "Go to Upper Window" })
+vim.keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window" })
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,44 @@
+return {
+  -- Plugin gợi ý và nhắc phím tắt thông minh
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- Lọc bỏ các gợi ý liên quan đến thao tác chuột (Mouse, Scroll...)
+      filter = function(mapping)
+        local lhs = mapping.lhs:lower()
+        if lhs:find("mouse") or lhs:find("scroll") then
+          return false
+        end
+        return true
+      end,
+      -- Cấu hình hiển thị ở góc dưới bên phải và nhỏ gọn
+      win = {
+        no_overlap = true,
+        width = 0.25, -- Chiều rộng 25% màn hình
+        height = { min = 5, max = 20 }, -- Chiều cao từ 5 đến 20 dòng
+        col = -1, -- Neo sát lề phải màn hình
+        row = -1, -- Neo sát lề dưới màn hình
+        border = "rounded", -- Viền bo tròn tinh tế
+        padding = { 1, 1 }, -- Padding bên trong nhỏ gọn
+      },
+      layout = {
+        width = { min = 50 }, -- Ép 1 cột dọc duy nhất
+        spacing = 3,
+      },
+    },
+    config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+
+      -- Đăng ký phím tắt Leader + ? để mở bảng tra cứu phím tắt nhanh
+      vim.keymap.set("n", "<leader>?", function()
+        wk.show()
+      end, { desc = "Show All Keymaps Help" })
+    end,
+  },
+}
