cc256ab633e10bcff10aa19cc75563f92496b952
Minh Ha <minhha7036@gmail.com>
2026-07-19 02:18:32 +0700

    feat: add keymaps for closing buffers safely

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,8 @@ vim.keymap.set("n", "<C-Left>", "<C-w>h", { desc = "Go to Left Window" })
 vim.keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower Window" })
 vim.keymap.set("n", "<C-Up>", "<C-w>k", { desc = "Go to Upper Window" })
 vim.keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window" })
+
+-- Quản lý Buffer (Tab)
+-- Đóng buffer hiện tại mà không làm hỏng bố cục cửa sổ split (Window splits)
+vim.keymap.set("n", "<leader>x", "<Cmd>bp|bd #<CR>", { desc = "Close Current Buffer" })
+vim.keymap.set("n", "<leader>bd", "<Cmd>bp|bd #<CR>", { desc = "Delete Buffer" })
