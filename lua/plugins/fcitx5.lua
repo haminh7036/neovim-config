666f8248b69f2e5af01be7c578c2a61526db73a8
Minh Ha <minhha7036@gmail.com>
2026-07-19 15:10:11 +0700

    fix(fcitx): disable lazy loading to ensure fcitx.nvim catches startup events

--- lua/plugins/fcitx5.lua
+++ lua/plugins/fcitx5.lua
@@ -2,6 +2,6 @@ return {
   "h-hg/fcitx.nvim",
   -- Load only if fcitx-remote or fcitx5-remote is executable
   cond = vim.fn.executable("fcitx5-remote") == 1 or vim.fn.executable("fcitx-remote") == 1,
-  -- Lazy load on VeryLazy to ensure it manages input state in Normal mode upon startup
-  event = "VeryLazy",
+  -- Load immediately at startup to ensure autocmds catch startup events
+  lazy = false,
 }
