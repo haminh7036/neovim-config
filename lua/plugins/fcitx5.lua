17d8747d822c8968640c60407d5500b6d4b6d190
Minh Ha <minhha7036@gmail.com>
2026-07-19 15:07:26 +0700

    fix(fcitx): switch to h-hg/fcitx.nvim to use fcitx5-remote and fix preedit issue

--- lua/plugins/fcitx5.lua
+++ lua/plugins/fcitx5.lua
@@ -1,10 +1,7 @@
 return {
-	"junyixu/fcitx.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("fcitx").setup({
-			-- enable_cmdline = true, -- also toggle when entering / or ?
-		})
-	end,
+  "h-hg/fcitx.nvim",
+  -- Load only if fcitx-remote or fcitx5-remote is executable
+  cond = vim.fn.executable("fcitx5-remote") == 1 or vim.fn.executable("fcitx-remote") == 1,
+  -- Lazy load on VeryLazy to ensure it manages input state in Normal mode upon startup
+  event = "VeryLazy",
 }
