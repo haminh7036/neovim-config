72c9cc6c9fe5a9129b7ab01289ba12dfa50ab31e
Minh Ha <66556512+haminh7036@users.noreply.github.com>
2026-07-24 15:38:03 +0700

    fix(blink): configure keymaps and preselection for autocompletion

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -7,7 +7,28 @@ return {
     opts = {
       keymap = { 
         preset = "default",
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback" },
         ["<C-y>"] = { "select_and_accept" },
+      },
+      completion = {
+        list = {
+          selection = {
+            preselect = function(ctx)
+              return ctx.mode ~= "cmdline"
+            end,
+            auto_insert = true,
+          },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+        },
       },
       appearance = {
         use_nvim_cmp_as_default = true,
@@ -17,5 +38,5 @@ return {
         default = { "lsp", "path", "snippets", "buffer" },
       },
     },
-  }
+  },
 }
