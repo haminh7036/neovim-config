02e02e7d850006762192331168b11a49d4774e9c
Minh Ha <minhha7036@gmail.com>
2026-08-16 03:13:55 +0700

    refactor(lazygit): replace lazygit.nvim with native floating terminal

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,11 +1,44 @@
+local function open_lazygit()
+  if vim.fn.executable("lazygit") ~= 1 then
+    vim.notify("lazygit is not installed or not in PATH", vim.log.levels.ERROR)
+    return
+  end
+
+  local width = math.floor(vim.o.columns * 0.9)
+  local height = math.floor(vim.o.lines * 0.9)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+
+  local buf = vim.api.nvim_create_buf(false, true)
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    style = "minimal",
+    border = "rounded",
+  })
+
+  vim.fn.termopen("lazygit", {
+    on_exit = function()
+      if vim.api.nvim_win_is_valid(win) then
+        vim.api.nvim_win_close(win, true)
+      end
+      if vim.api.nvim_buf_is_valid(buf) then
+        vim.api.nvim_buf_delete(buf, { force = true })
+      end
+    end,
+  })
+  vim.cmd("startinsert")
+end
+
 return {
   {
-    "kdheepak/lazygit.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
+    "native-lazygit",
+    dir = vim.fn.stdpath("config"),
     keys = {
-      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+      { "<leader>gg", open_lazygit, desc = "LazyGit" },
     },
   },
 }
